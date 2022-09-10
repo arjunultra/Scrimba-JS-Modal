dfc694cc50a234e6c683500e48cc1047c262a76e
S Arjun Arasu <90092043+arjunultra@users.noreply.github.com>
2022-09-10 11:31:47 +0530

    completedModal

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const openModal = document.getElementById("open-modal");
 const closeBtn = document.getElementById("close-btn");
 const modal = document.getElementById("modal");
+const overlay = document.getElementById("overlay");
+const container = document.getElementById("container");
 modal.style.display = "none";
 openModal.addEventListener("click", function () {
   modal.style.display = "block";
+  overlay.style.display = "none";
+  container.style.backgroundColor = "black";
 });
 closeBtn.addEventListener("click", function () {
   modal.style.display = "none";
+  overlay.style.display = "block";
+  container.style.backgroundColor = "plum";
 });
